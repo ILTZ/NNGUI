1ec05203e2555d13132e9bef91852537481d1a06
ILTZ <63543530+ILTZ@users.noreply.github.com>
2021-09-20 18:50:21 +0500

    -

--- source/main.py
+++ source/main.py
@@ -1,49 +1,19 @@
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 import sys
 import GUI
-import os
+
 
 
 #Disable DPI scaling
 import ctypes
 awareness = ctypes.c_int()
-errorCode = ctypes.windll.shcore.GetProcessDpiAwareness(0, ctypes.byref(awareness))
-print(awareness.value)
 errorCode = ctypes.windll.shcore.SetProcessDpiAwareness(0) 
-errorCode = ctypes.windll.shcore.GetProcessDpiAwareness(0, ctypes.byref(awareness))
-print(awareness.value)
+
 ###################################################################################
 
-def setScaleDPI():
-
-    pass
-
-
 def main():
-    
-
-    os.environ["QT_DEVICE_PIXEL_RATIO"] = "0"
-    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
-    os.environ["QT_SCREEN_SCALE_FACTORS"] = "1"
-    os.environ["QT_SCALE_FACTOR"] = "0"
-
-    
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_UseHighDpiPixmaps, True)
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_DisableHighDpiScaling, True)
-
-    
     app = QtWidgets.QApplication(sys.argv)
-    
-    
-    
-
-    
-
     window = GUI.GUImm()
-    
-
-    
-
     #window.show()
     app.exec_()
 
